b301d74ca3667f178a29e0007855153b7d5cc1ae
Wobin <wobster@gmail.com>
2012-09-22 06:55:18 +1000

    Munging to work with heroku

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-require('derby').run(__dirname + '/lib/server')
+var port = process.env.PORT || 3000;
+if (process.env.NODE_ENV === 'debug' || process.env.NODE_ENV === 'production') {
+    // "up" module interferes with node debug & node-inspector. also, nodejitsu (aka, 'production') doesn't like up
+    console.log('Running in ' + process.env.NODE_ENV + ': require("./lib/server").listen(' + port + ');');
+    require('./lib/server').listen(port);
+} else {
+    require('derby').run(__dirname + '/lib/server', port);
+}
