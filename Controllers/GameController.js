1a23fa2316035fee1507d2684b4e802d33be1d27
Wobin <wobster@gmail.com>
2012-10-09 00:29:51 +1100

    Now for the rest of the pack

--- Controllers/GameController.js
+++ Controllers/GameController.js
@@ -5,7 +5,18 @@ var GameController;
         CardController.removeAllCards();
         _.each(BaseCards.WhiteCards, function (card) {
             CardController.addCard(card, "White");
-            console.log("Adding Card: " + card);
+            console.log("Adding White Card: " + card);
+        });
+        _.each(BaseCards.BlackCards, function (card) {
+            CardController.addCard(card, "Black");
+            console.log("Adding Black Card: " + card);
+        });
+        _.each(BaseCards.MultiBlackCards, function (card) {
+            var newCard = CardController.addCard(card.Line, "Black");
+            console.log("Adding MultiCard: " + card.Line);
+            _.each(card.Conditions, function (condition) {
+                CardController.addCondition(newCard, condition.Condition, condition.Value);
+            });
         });
     }
     GameController.ReImportCards = ReImportCards;
